a439822aaae72a9ad73f7b49178e1b2723c6abd4
Mahmoud <mahwafa2005@gmail.com>
2020-03-28 12:50:39 +0300

    first release

---
+++ dsl-job/build.groovy
@@ -0,0 +1,16 @@
+job('First-DSL-job'){
+  description("Create a first DSL job to run inside jenkins on ${new Date()}")
+  scm{
+    git("https://github.com/mahmoudwaf/dsl-job.git",master)
+  }
+
+  triggers {
+    scm('* * * * *')
+  }
+  steps{
+    maven('clean package','dsl-job/pom.xml')
+  }
+  publisher{
+    archiveArtifacts '**/*.jar'
+  }
+}
